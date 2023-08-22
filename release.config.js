9faba8dc48d8c6d07ce388f6cfdb99ea9f6ba3e2
Ruben Romero Montes <rromerom@redhat.com>
2023-08-22 17:19:33 +0200

    fix: publish package locally
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- release.config.js
+++ release.config.js
@@ -1,63 +1,45 @@
 module.exports = {
-  prepare: [
+  branches: [
+    "master"
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
     "@semantic-release/npm",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["bin/**"]
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ],
+  prepare: [
     {
       // build the Linux executable
       "path": "@semantic-release/exec",
-      "cmd": "pkg . -t node12-linux-x64 -o analytics-lsp-linux"
+      "cmd": "pkg . -t node18-linux-x64 -o bin/analytics-lsp-linux"
     },
     {
       // build the macOS executable
       "path": "@semantic-release/exec",
-      "cmd": "pkg . -t node12-macos-x64 -o analytics-lsp-macos"
+      "cmd": "pkg . -t node18-macos-x64 -o bin/analytics-lsp-macos"
     },
     {
       // build the windows executable
       "path": "@semantic-release/exec",
-      "cmd": "pkg . -t node12-win-x64 -o analytics-lsp-win.exe"
+      "cmd": "pkg . -t node18-win-x64 -o bin/analytics-lsp-win.exe"
     },
     {
       // shasum
       "path": "@semantic-release/exec",
-      "cmd": "shasum -a 256 analytics-lsp-linux > analytics-lsp-linux.sha256 && shasum -a 256 analytics-lsp-macos > analytics-lsp-macos.sha256 && shasum -a 256 analytics-lsp-win.exe > analytics-lsp-win.sha256"
+      "cmd": "shasum -a 256 analytics-lsp-linux > bin/analytics-lsp-linux.sha256 && shasum -a 256 analytics-lsp-macos > bin/analytics-lsp-macos.sha256 && shasum -a 256 analytics-lsp-win.exe > bin/analytics-lsp-win.sha256"
     },
-  ],
-  publish: [
-    {
-      path: "@semantic-release/github",
-      assets: [
-        {
-          path: "analytics-lsp-linux",
-          name: "analytics-lsp-linux",
-          label: "analytics-lsp-linux"
-        },
-        {
-          path: "analytics-lsp-macos",
-          name: "analytics-lsp-macos",
-          label: "analytics-lsp-macos"
-        },
-        {
-          path: "analytics-lsp-win.exe",
-          name: "analytics-lsp-win.exe",
-          label: "analytics-lsp-win.exe"
-        },
-        {
-          path: "analytics-lsp-linux.sha256",
-          name: "analytics-lsp-linux.sha256",
-          label: "analytics-lsp-linux.sha256"
-        },
-        {
-          path: "analytics-lsp-macos.sha256",
-          name: "analytics-lsp-macos.sha256",
-          label: "analytics-lsp-macos.sha256"
-        },
-        {
-          path: "analytics-lsp-win.sha256",
-          name: "analytics-lsp-win.sha256",
-          label: "analytics-lsp-win.sha256"
-        },
-      ]
-    },
-    "@semantic-release/npm",
   ]
 }
